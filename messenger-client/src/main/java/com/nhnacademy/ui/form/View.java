59f2b28f631effa9db01eb02a3f57680f0e67029
{dabin5682} <dabin5682@gmail.com>
2026-01-30 10:57:04 +0900

    로그인,로그아웃 구현

--- messenger-client/src/main/java/com/nhnacademy/ui/form/View.java
+++ messenger-client/src/main/java/com/nhnacademy/ui/form/View.java
@@ -1,7 +1,8 @@
-package com.nhnacademy.ui;
+package com.nhnacademy.ui.form;
 
 public interface View {
     void mainView();
     void showRoomList();
     void showEnterRoom();
+    void logout();
 }
