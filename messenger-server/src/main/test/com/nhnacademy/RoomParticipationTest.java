7885b98f90afe68c8529564038036c83d9a1c790
Poly-Etilen <wlsdud04060@naver.com>
2026-01-30 11:07:17 +0900

    test: test 코드 작성

--- messenger-server/src/main/test/com/nhnacademy/RoomParticipationTest.java
+++ messenger-server/src/main/test/com/nhnacademy/RoomParticipationTest.java
@@ -2,10 +2,12 @@ package com.nhnacademy;
 
 import com.nhnacademy.command.impl.CreateRoomCommand;
 import com.nhnacademy.command.impl.JoinRoomCommand;
+import com.nhnacademy.command.impl.RoomUserListCommand;
 import com.nhnacademy.domain.Header.MessageType;
 import com.nhnacademy.domain.Message;
 import com.nhnacademy.manager.ChatRoomManager;
 import com.nhnacademy.model.ChatRoom;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,26 @@ public class RoomParticipationTest extends ServerTestSupport{
         ChatRoom room = ChatRoomManager.getInstance().createRoom("Game Room");
         JoinRoomCommand command = new JoinRoomCommand();
         Message message = createMessage(MessageType.JOIN_ROOM, Map.of("roomId", room.getId()));
+
+        command.execute(session, message);
+
+        Assertions.assertTrue(room.getSessions().contains(session));
+        Assertions.assertTrue(out.size() > 0, "입장 성공 메시지가 전송되어야 함");
+    }
+
+    @Test
+    @DisplayName("방 유저 목록 조회: 현재 방 인원 정보 전송")
+    void roomUserListTest() {
+        ChatRoom room = ChatRoomManager.getInstance().createRoom("Dev Room");
+        session.setUserId("marco");
+        session.setCurrentRoomId(room.getId());
+        room.addSession(session);
+
+        RoomUserListCommand command = new RoomUserListCommand();
+        Message message = createMessage(MessageType.ROOM_USER_LIST, Map.of("roomId", room.getId()));
+
+        command.execute(session, message);
+
+        Assertions.assertTrue(out.size() > 0, "유저 목록이 전송되어야 함");
     }
 }
